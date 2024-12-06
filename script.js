2c06fd6f8d064df06a8678f8900a9d5075f272d5
fernandovidal <fernando07.vidal@outlook.com>
2024-12-06 15:51:11 -0300

    Curso de Logica de Programação

---
+++ script.js
@@ -0,0 +1,49 @@
+//Saida
+console.log("Digite o nome do seu jogador");
+
+//Declarando uma variável
+let nikename = "";
+
+//Concaternando uma menssagem fixa + uma variável
+console.log ("Bem vindo" + nikename);
+
+console.log(nikename + "Entrou no servidor");
+
+Const.notificacao = "Pokemon Go Diz:";
+
+console.log(notificacao + "tem um novo pokemon rigião");
+console.log(notificacao + "você foi derrotado por um lider");
+
+let poteCafe = "café pilão"
+let poteAcucar = "Açucar Refinado"
+let poteBiscoito = "Biscoito de Maizena"
+Const.messagemDaVovo = "Na cozinha da Vovó hoje tem:"
+
+poteCafe = "Café Caiçara"
+poteAcucar = "Açucar União"
+poteBiscoito = "Biscoito da Triunfo"
+
+console.log(messagemDaVovo + poteCafe + " - "
+    poteAcucar + " = "
+    poteBiscoito + " _ "
+)
+
+//Pokemon
+let namePokemon = "Pikachu"
+let levelPokemon = 20
+let pontosDeVidaPokemon = 45
+let generopokemon = "M"
+let selecionavel = true
+
+
+//nome, idade, numero de telefone, cpf, endereço.
+//string = armazenar texto 
+//number = armazenar numeros
+//booLean = armazenar valores lógicos
+
+let namePessoa = "Maria"
+let idade = 80
+let numeroDeTelefone = "(11)9.2002-3112"
+let cpf = "450.708.778-31"
+let endereco = "Rua da Boemia, 051, Morro Santa Tereza"
+let recebePensao = true
